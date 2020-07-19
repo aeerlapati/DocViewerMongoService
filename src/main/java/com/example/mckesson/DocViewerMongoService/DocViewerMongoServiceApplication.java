e88fc159464a3a12c36af57ab6ba9bf0236e07bb
abhinav <abhinav.eerlapati@mckesson.com>
2020-07-19 13:58:52 -0700

    Project Skeleton

---
+++ src/main/java/com/example/mckesson/DocViewerMongoService/DocViewerMongoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mckesson.DocViewerMongoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocViewerMongoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocViewerMongoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mckesson/DocViewerMongoService/DocViewerMongoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mckesson.DocViewerMongoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocViewerMongoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
